98856b1b365ef98cd83cb8be8cc5ff6287474e01
Koen Klaren <koen.klaren@gmail.com>
2019-09-23 00:02:32 +0200

    Some experimentation

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 int main() {
     Curve<double> curve1({{0, 0}, {1, 0}});
 
-    Curve<double> curve2({{.5, .5}, {1.5, .5}});
+    Curve<double> curve2({{0.5, 0.5}, {1.5, 0.5}});
 
-    FastMarchCDTW::compute(curve1, curve2, 0.1, true);
+    std::cout << FastMarchCDTW::compute(curve1, curve2, 0.05, true) << std::endl;
 
     return 0;
 }
