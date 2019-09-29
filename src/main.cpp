24fa1da8de89d82effbddced895f1ae66e5ef447
Koen Klaren <koen.klaren@gmail.com>
2019-09-29 21:16:43 +0200

    Save center ratios at which min sum/diff was found

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,27 @@ int main() {
     std::cout << "curve1 distance to curve2: " << solver.computeDistance() << std::endl;
     std::cout << std::endl;
     solver.computeMatching(0.1);
-    solver.computeCenter(0.5815); // Manually picked center with same distance to curve1 and curve2
     solver.save();
 
-    const int numCenters = 21;
+    const int numCenters = 51;
+    std::vector<double> centerRatios;
     std::vector<Curve<double>> centers;
 
     for (int i = 0; i < numCenters; ++i) {
-        solver.computeCenter((double) i / (numCenters - 1));
+        double ratio = (double) i / (numCenters - 1);
+        solver.computeCenter(ratio);
         centers.push_back(Curve<double>(solver.getCenter()));
+        centerRatios.push_back(ratio);
     }
 
     arma::mat centerDistances(centers.size(), 2);
 
     double minSum = INFINITY;
+    double minSumRatio = 0;
     Curve<double> minSumCenter({});
 
     double minDiff = INFINITY;
+    double minDiffRatio = 0;
     Curve<double> minDiffCenter({});
 
     for (int i = 0; i < centers.size(); ++i) {
@@ -46,15 +50,18 @@ int main() {
         if (sum < minSum) {
             minSum = sum;
             minSumCenter = center;
+            minSumRatio = centerRatios[i];
         }
 
         double diff = std::abs(centerDistances(i, 0) - centerDistances(i, 1));
         if (diff < minDiff) {
             minDiff = diff;
             minDiffCenter = center;
+            minDiffRatio = centerRatios[i];
         }
     }
 
+    arma::rowvec({minSumRatio, minDiffRatio}).save("minCenterValues.csv",arma::csv_ascii);
     minSumCenter.getVertices().save("minSumCenter.csv", arma::csv_ascii);
     minDiffCenter.getVertices().save("minDiffCenter.csv", arma::csv_ascii);
     centerDistances.save("centerDistances.csv", arma::csv_ascii);
