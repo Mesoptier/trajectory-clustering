c1413485e448c87ba48bad7961033fa2d01fe0e8
Koen Klaren <koen.klaren@gmail.com>
2019-12-09 19:51:58 +0100

    Add methods for reading curves from file

--- src/io.cpp
+++ src/io.cpp
@@ -2,6 +2,30 @@
 
 #define FILE_ERROR(filename) throw std::runtime_error("Failed to open file " + filename)
 
+void io::readCurve(const std::string& filename, Curve& curve, int header_size) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        FILE_ERROR(filename);
+    }
+
+    distance_t x, y;
+    while (file >> x >> y) {
+        // Ignore duplicate coordinates
+        if (!curve.empty() && curve.back().x == x && curve.back().y == y) {
+            continue;
+        }
+        curve.push_back({x, y});
+    }
+
+    file.close();
+}
+
+Curve io::readCurve(const std::string& filename, int header_size) {
+    Curve curve;
+    readCurve(filename, curve, header_size);
+    return curve;
+}
+
 void io::exportPoints(const std::string& filename, const Points& points) {
     std::ofstream file(filename);
     if (!file.is_open()) {
--- src/io.h
+++ src/io.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "geom.h"
+#include "Curve.h"
 
 namespace io {
+    void readCurve(const std::string& filename, Curve& curve, int header_size = 0);
+    Curve readCurve(const std::string& filename, int header_size = 0);
     void exportPoints(const std::string& filename, const Points& points);
 }
