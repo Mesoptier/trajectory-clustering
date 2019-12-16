faf0f2befbfa0441759d8a1149d6dc7d36ea7777
Koen Klaren <koen.klaren@gmail.com>
2019-12-16 12:16:01 +0100

    Use boost timer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <chrono>
+#include <boost/timer/timer.hpp>
 #include <iostream>
 
 #include "io.h"
@@ -6,25 +6,27 @@
 #include "IntegralFrechet/IntegralFrechet.h"
 
 int main() {
-//    const Curve curve1({{0, 0}, {1, 1}, {2, 2}});
-//    const Curve curve2({{0, 1}, {2, 1}});
+    boost::timer::auto_cpu_timer timer;
 
     const Curve curve1 = io::readCurve("data/characters/data/a0001.txt");
     const Curve curve2 = io::readCurve("data/characters/data/a0002.txt");
 
-    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "reading curves:";
+    timer.report();
+    timer.start();
 
     IntegralFrechet alg(curve1, curve2);
     distance_t cost;
     Points matching;
     std::tie(cost, matching) = alg.compute_matching();
 
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = finish - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::cout << "compute matching:";
+    timer.report();
+    timer.start();
 
-//    std::cout << "COST: " << cost << "\n";
-//    std::cout << "PATH: ";
+    std::cout << "cost: " << cost << "\n";
+    std::cout << "matching size: " << matching.size() << "\n";
+//    std::cout << "matching: ";
 //    for (auto p : matching) std::cout << "\n| " << p;
 //    std::cout << std::endl;
 
@@ -32,98 +34,6 @@ int main() {
     io::exportPoints("data/out/curve2.csv", curve2.get_points());
     io::exportPoints("data/out/matching.csv", matching);
 
-//    double h = .1;
-//
-//    Solver solver(curve1, curve2, h, ImageMetric::L2, ParamMetric::LInfinity_NoShortcuts);
-//    auto matching = solver.getMatching();
-//
-//    std::cout << "Distance: " << solver.getDistance() << std::endl;
-//    std::cout << matching << std::endl;
-//
-//    // Solver summary
-//    std::ofstream solverFile;
-//    solverFile.open("solver.xml");
-//
-//    ExpressionML::Writer writer(solverFile);
-//    writer.open();
-//    solver.writeExpressionML(writer);
-//    writer.close();
-//
-//    solverFile.close();
-
-
-//    double h = 0.01;
-//    int imageNorm = 2;
-//    int paramNorm = 1;
-//
-//    FastMarchIntegralFrechet solver(curve1, curve2, h, imageNorm, paramNorm);
-//
-//    std::cout << "curve1 distance to curve2: " << solver.computeDistance() << std::endl;
-//    std::cout << std::endl;
-//    solver.computeMatching(0.01);
-//    solver.save();
-//
-//    const int numCenters = 51;
-//    std::vector<double> centerRatios;
-//    std::vector<Curve<double>> centers;
-//
-//    for (int i = 0; i < numCenters; ++i) {
-//        double ratio = (double) i / (numCenters - 1);
-//        solver.computeCenter(ratio);
-//        centers.push_back(Curve<double>(solver.getCenter()));
-//        centerRatios.push_back(ratio);
-//    }
-//
-//    arma::mat centerDistances(centers.size(), 2);
-//
-//    double minSum = INFINITY;
-//    double minSumRatio = 0;
-//    Curve<double> minSumCenter({});
-//
-//    double minDiff = INFINITY;
-//    double minDiffRatio = 0;
-//    Curve<double> minDiffCenter({});
-//
-//    double minSumSquares = INFINITY;
-//    double minSumSquaresRatio = 0;
-//    Curve<double> minSumSquaresCenter({});
-//
-//    for (int i = 0; i < centers.size(); ++i) {
-//        const auto& center = centers[i];
-//
-//        solver = FastMarchIntegralFrechet(curve1, center, h, imageNorm, paramNorm);
-//        centerDistances(i, 0) = solver.computeDistance();
-//
-//        solver = FastMarchIntegralFrechet(curve2, center, h, imageNorm, paramNorm);
-//        centerDistances(i, 1) = solver.computeDistance();
-//
-//        double sum = centerDistances(i, 0) + centerDistances(i, 1);
-//        if (sum < minSum) {
-//            minSum = sum;
-//            minSumCenter = center;
-//            minSumRatio = centerRatios[i];
-//        }
-//
-//        double diff = std::abs(centerDistances(i, 0) - centerDistances(i, 1));
-//        if (diff < minDiff) {
-//            minDiff = diff;
-//            minDiffCenter = center;
-//            minDiffRatio = centerRatios[i];
-//        }
-//
-//        double sumSquares = pow(centerDistances(i, 0), 2) + pow(centerDistances(i, 1), 2);
-//        if (sumSquares < minSumSquares) {
-//            minSumSquares = sumSquares;
-//            minSumSquaresCenter = center;
-//            minSumSquaresRatio = centerRatios[i];
-//        }
-//    }
-//
-//    arma::rowvec({minSumRatio, minDiffRatio, minSumSquaresRatio}).save("minCenterValues.csv",arma::csv_ascii);
-//    minSumCenter.getVertices().save("minSumCenter.csv", arma::csv_ascii);
-//    minDiffCenter.getVertices().save("minDiffCenter.csv", arma::csv_ascii);
-//    minSumSquaresCenter.getVertices().save("minSumSquaresCenter.csv", arma::csv_ascii);
-//    centerDistances.save("centerDistances.csv", arma::csv_ascii);
-
+    std::cout << "total:";
     return 0;
 }
