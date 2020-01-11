616b066a930505a37fab26ed15e28813b913985e
Koen Klaren <koen.klaren@gmail.com>
2020-01-11 12:58:02 +0100

    Remove Boost

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <boost/timer/timer.hpp>
 #include <iostream>
 
 #include "io.h"
@@ -6,23 +5,13 @@
 #include "IntegralFrechet/IntegralFrechet.h"
 
 int main() {
-    boost::timer::auto_cpu_timer timer;
-
     const Curve curve1 = io::readCurve("data/characters/data/a0001.txt");
     const Curve curve2 = io::readCurve("data/characters/data/a0002.txt");
-
-    std::cout << "reading curves:";
-    timer.report();
-    timer.start();
 
     IntegralFrechet alg(curve1, curve2);
     distance_t cost;
     Points matching;
     std::tie(cost, matching) = alg.compute_matching();
-
-    std::cout << "compute matching:";
-    timer.report();
-    timer.start();
 
     std::cout << "cost: " << cost << "\n";
     std::cout << "matching size: " << matching.size() << "\n";
@@ -34,6 +23,5 @@ int main() {
     io::exportPoints("data/out/curve2.csv", curve2.get_points());
     io::exportPoints("data/out/matching.csv", matching);
 
-    std::cout << "total:";
     return 0;
 }
