47adaf6c46d8749dc3313a79a94179f2dce88df9
Koen Klaren <koen.klaren@gmail.com>
2020-02-05 23:38:07 +0100

    Add SymmetricMatrix class

---
+++ src/SymmetricMatrix.cpp
@@ -0,0 +1,35 @@
+#include "SymmetricMatrix.h"
+
+void SymmetricMatrix::write(std::ofstream& file, unsigned int precision) const {
+    file << "%%MatrixMarket matrix array real symmetric\n";
+    file << n << ' ' << n << '\n';
+    file.precision(precision);
+    for (double value : data) {
+        file << value << '\n';
+    }
+}
+
+SymmetricMatrix SymmetricMatrix::read(std::ifstream& file) {
+    // Parse header
+    std::string line;
+    std::getline(file, line);
+    if (line != "%%MatrixMarket matrix array real symmetric") {
+        throw std::runtime_error("unsupported header");
+    }
+
+    // Parse matrix size
+    size_t m, n;
+    file >> m >> n;
+    if (m != n) {
+        throw std::runtime_error("matrix is not square");
+    }
+
+    // Parse values
+    SymmetricMatrix matrix(n);
+    double value;
+    while (file >> value) {
+        matrix.data.push_back(value);
+    }
+
+    return matrix;
+}
---
+++ src/SymmetricMatrix.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+
+/**
+ * Stores a N x N symmetric matrix in a flat array of size (N*(N+1))/2.
+ */
+class SymmetricMatrix
+{
+    size_t n;
+    std::vector<double> data;
+
+    /**
+     * Get the index in the data array from two coordinates.
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    inline size_t idx(size_t i, size_t j) const {
+        if (i <= j) {
+            return i * n - (i * (i + 1)) / 2 + j;
+        } else {
+            return j * n - (j * (j + 1)) / 2 + i;
+        }
+    }
+
+public:
+    explicit SymmetricMatrix(size_t n) : n(n), data((n * (n + 1)) / 2, 0) {}
+
+    /**
+     * Access the element at (i, j).
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    double& at(size_t i, size_t j) {
+        return data.at(idx(i, j));
+    }
+
+    /**
+     * Write the matrix to a file.
+     *
+     * @param file
+     * @param precision
+     */
+    void write(std::ofstream& file, unsigned int precision = 10) const;
+
+    /**
+     * Read a matrix from a file.
+     *
+     * @param file
+     * @return
+     */
+    static SymmetricMatrix read(std::ifstream& file);
+
+    bool operator==(const SymmetricMatrix& rhs) const {
+        return n == rhs.n && data == rhs.data;
+    }
+};
