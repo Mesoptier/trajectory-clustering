f191ca9d9af88574c2fb94c81ae5f89c17359a05
Koen Klaren <koen.klaren@gmail.com>
2020-02-09 20:59:42 +0100

    Experiment with band vs. without band

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
+#include <random>
 
 #include "src/io.h"
 #include "src/Curve.h"
@@ -107,7 +108,7 @@ IntegralFrechet::MatchingResult compute_matching(const Curve& curve1, const Curv
 }
 
 MatchingBand compute_band(const Curve& curve1, const Curve& curve2, const Points& matching, distance_t radius) {
-    auto start_time = std::chrono::high_resolution_clock::now();
+//    auto start_time = std::chrono::high_resolution_clock::now();
 
     const MatchingBand band(curve1, curve2, matching, radius);
 
@@ -118,6 +119,72 @@ MatchingBand compute_band(const Curve& curve1, const Curve& curve2, const Points
     return band;
 }
 
+//
+// Experiments
+//
+
+void experiment_with_or_without_bands() {
+    struct Stat {
+        distance_t band_cost;
+        long long int band_time;
+        distance_t no_band_cost;
+        long long int no_band_time;
+    };
+    std::vector<Stat> stats;
+
+    bool maintain_lengths = true;
+    distance_t simple_resolution = 10;
+    distance_t resolution = 1;
+    distance_t band_radius = 1;
+
+    const auto curves = read_curves("data/characters/data");
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    seed = 4067390372;
+    std::cout << seed << std::endl;
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<size_t> distribution(0, curves.size() - 1);
+
+    size_t k_max = 1000;
+    for (size_t k = 0; k < k_max; ++k) {
+        auto i = distribution(generator);
+        auto j = distribution(generator);
+
+        const auto curve1 = curves.at(i);
+        const auto curve2 = curves.at(j);
+
+        // With band
+        auto start_band = std::chrono::high_resolution_clock::now();
+        const auto result_alt = compute_matching(curve1.simplify(maintain_lengths), curve2.simplify(maintain_lengths), simple_resolution,nullptr);
+        const auto band = compute_band(curve1, curve2, result_alt.matching, band_radius);
+        const auto result_band = compute_matching(curve1, curve2, resolution, &band);
+        auto end_band = std::chrono::high_resolution_clock::now();
+        auto duration_band = std::chrono::duration_cast<std::chrono::milliseconds>(end_band - start_band).count();
+
+        // Without band
+        auto start_no_band = std::chrono::high_resolution_clock::now();
+        const auto result_no_band = compute_matching(curve1, curve2, resolution, nullptr);
+        auto end_no_band = std::chrono::high_resolution_clock::now();
+        auto duration_no_band = std::chrono::duration_cast<std::chrono::milliseconds>(end_no_band - start_no_band).count();
+
+        stats.push_back({
+            result_band.cost,
+            duration_band,
+            result_no_band.cost,
+            duration_no_band,
+        });
+
+        std::cout << (k + 1) << " / " << k_max << '\n';
+    }
+
+    std::ofstream file("data/out/band_stats.tsv");
+    file << "band_cost\tband_time\tno_band_cost\tno_band_time\n";
+    for (const auto& stat : stats) {
+        file << stat.band_cost << '\t' << stat.band_time << '\t' << stat.no_band_cost << '\t' << stat.no_band_time << '\n';
+    }
+    file.close();
+}
+
 int main() {
 //    const auto distance_matrix = read_matrix("data/out/distance_matrix.mtx");
 //    compute_clusters(distance_matrix, 19); // "characters" has 19 classes
@@ -126,32 +193,7 @@ int main() {
 //    const auto distance_matrix = compute_distance_matrix(curves);
 //    export_matrix(distance_matrix, "data/out/distance_matrix.mtx");
 
-    bool maintain_lengths = true;
-
-    const auto curve1 = io::read_curve("data/characters/data/b1559.txt");
-    const auto curve2 = io::read_curve("data/characters/data/p0867.txt");
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    const auto result_alt = compute_matching(curve1.simplify(maintain_lengths), curve2.simplify(maintain_lengths), 10,nullptr);
-    const auto band = compute_band(curve1, curve2, result_alt.matching, 2);
-    const auto result = compute_matching(curve1, curve2, 1, &band);
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-    std::cout << "main: " << duration << "ms\n";
-
-    io::export_points("data/out/curve1.csv", curve1.get_points());
-    io::export_points("data/out/curve2.csv", curve2.get_points());
-    io::export_points("data/out/matching.csv", result.matching);
-    io::export_points("data/out/matching_alt.csv", result_alt.matching);
-
-    std::cout << "matching cost: " << result.cost << '\n';
-    std::cout << "matching (alt) cost: " << result_alt.cost << '\n';
-
-    const auto result_no_band = compute_matching(curve1, curve2, 1, nullptr);
-    io::export_points("data/out/matching_alt.csv", result_no_band.matching);
-    std::cout << "matching (no band) cost: " << result_no_band.cost << '\n';
+    experiment_with_or_without_bands();
 
     return 0;
 }
