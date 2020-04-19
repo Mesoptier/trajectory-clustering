42d0891e9baa08e9bf5fe7307f8003ac2d82e152
Koen Klaren <koen.klaren@gmail.com>
2020-04-19 17:34:14 +0200

    Start adding tests for floating point errors

--- Google_tests/NaiveLowerEnvelopeTest.cpp
+++ Google_tests/NaiveLowerEnvelopeTest.cpp
@@ -1,29 +1,7 @@
 #include "gtest/gtest.h"
 #include "../src/cdtw/naive_lower_envelope.h"
 
-void AssertIntervalEqual(const Interval& actual, const Interval& expected) {
-    ASSERT_DOUBLE_EQ(actual.min, expected.min);
-    ASSERT_DOUBLE_EQ(actual.max, expected.max);
-}
-
-template<size_t D>
-void AssertPolynomialEqual(const Polynomial<D>& actual, const Polynomial<D>& expected) {
-    for (size_t d = 0; d <= D; ++d) {
-        ASSERT_DOUBLE_EQ(actual.coefficients.at(d), expected.coefficients.at(d));
-    }
-}
-
-template<size_t D>
-void AssertPiecewisePolynomialEqual(const PiecewisePolynomial<D>& actual, const PiecewisePolynomial<D>& expected) {
-    ASSERT_EQ(actual.pieces.size(), expected.pieces.size());
-    for (size_t i = 0; i < actual.pieces.size(); ++i) {
-        const PolynomialPiece<D>& actual_piece = actual.pieces.at(i);
-        const PolynomialPiece<D>& expected_piece = expected.pieces.at(i);
-
-        AssertIntervalEqual(actual_piece.interval, expected_piece.interval);
-        AssertPolynomialEqual(actual_piece.polynomial, expected_piece.polynomial);
-    }
-}
+constexpr double EPS = 1e-11;
 
 // TODO, test the following:
 //  - Pieces with identical polynomials
@@ -108,6 +86,29 @@ TEST(NaiveLowerEnvelopeTest, IdenticalPolynomials) {
         const auto actual = naive_lower_envelope(pieces);
         const PiecewisePolynomial<2> expected({
             {{0, 3}, Polynomial<2>({3, 2, 1})},
+        });
+        ASSERT_EQ(actual, expected);
+    }
+    { // Order: left, right
+        const std::vector<PolynomialPiece<2>> pieces {
+            {{0 + EPS, 2}, Polynomial<2>({3, 2, 1})},
+            {{0, 3}, Polynomial<2>({3, 2, 1})},
+        };
+        const auto actual = naive_lower_envelope(pieces);
+        const PiecewisePolynomial<2> expected({
+            {{0, 3}, Polynomial<2>({3, 2, 1})},
+        });
+        ASSERT_EQ(actual, expected);
+    }
+    { // Order: left, right
+        const std::vector<PolynomialPiece<2>> pieces {
+            {{0, 2}, Polynomial<2>({3, 2, 1})},
+            {{0 + EPS, 3}, Polynomial<2>({3, 2, 1})},
+        };
+        const auto actual = naive_lower_envelope(pieces);
+        const PiecewisePolynomial<2> expected({
+            {{0, 0 + EPS}, Polynomial<2>({3, 2, 1})},
+            {{0 + EPS, 3}, Polynomial<2>({3, 2, 1})},
         });
         ASSERT_EQ(actual, expected);
     }
