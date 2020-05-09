6e2ba109280606ce785e66d89371e06bbd29f51c
Koen Klaren <koen.klaren@gmail.com>
2020-05-09 21:28:05 +0200

    Add find_roots_y and embed_x methods for cubix functions

--- src/cdtw/BivariatePolynomial.h
+++ src/cdtw/BivariatePolynomial.h
@@ -62,6 +62,11 @@ struct BivariatePolynomial
         return result;
     }
 
+    /**
+     * Returns f(g(y), y)
+     */
+    Polynomial<D> embed_x(const Polynomial<1>& g) const;
+
     BivariatePolynomial<D> translate_xy(double x, double y) const;
 
     //
@@ -89,6 +94,10 @@ struct BivariatePolynomial
  * @param f
  * @return
  */
+template<size_t D>
+std::vector<Polynomial<1>> find_roots_y(const BivariatePolynomial<D>& f);
+
+template<>
 std::vector<Polynomial<1>> find_roots_y(const BivariatePolynomial<1>& f) {
     auto c00 = f.coefficients[0][0];
     auto c01 = f.coefficients[0][1];
@@ -101,6 +110,29 @@ std::vector<Polynomial<1>> find_roots_y(const BivariatePolynomial<1>& f) {
     return {
         Polynomial<1>({-c00 / c10, -c01 / c10})
     };
+}
+
+template<>
+std::vector<Polynomial<1>> find_roots_y(const BivariatePolynomial<2>& f) {
+    // TODO: Return roots where possible (i.e. anything without square roots)
+
+//    std::cout << f << std::endl;
+
+    auto c00 = f.coefficients[0][0];
+    auto c01 = f.coefficients[0][1];
+    auto c02 = f.coefficients[0][2];
+    auto c10 = f.coefficients[1][0];
+    auto c11 = f.coefficients[1][1];
+    auto c20 = f.coefficients[2][0];
+
+//    if (c00 == 0 && c10 == 0 && c20 == 0) {
+//        return find_roots_y(BivariatePolynomial<1>({{
+//            {c01, c02},
+//            {c11, 0},
+//        }}));
+//    }
+
+    return {};
 }
 
 template<>
@@ -117,6 +149,51 @@ BivariatePolynomial<2> BivariatePolynomial<2>::translate_xy(double cx, double cy
         {{c10 - 2*c20*cx - c11*cy, c11, 0}},
         {{c20, 0, 0}},
     }});
+}
+
+// TODO: Generalize embed_x
+
+template<>
+Polynomial<2> BivariatePolynomial<2>::embed_x(const Polynomial<1>& g) const {
+    auto c00 = coefficients[0][0];
+    auto c01 = coefficients[0][1];
+    auto c02 = coefficients[0][2];
+    auto c10 = coefficients[1][0];
+    auto c20 = coefficients[2][0];
+    auto c11 = coefficients[1][1];
+
+    auto g0 = g.coefficients[0];
+    auto g1 = g.coefficients[1];
+
+    return Polynomial<2>({
+        c00 + c01*g0 + c02*g0*g0,
+        c10 + c11*g0 + c01*g1 + 2*c02*g0*g1,
+        c20 + c11*g1 + c02*g1*g1,
+    });
+}
+
+template<>
+Polynomial<3> BivariatePolynomial<3>::embed_x(const Polynomial<1>& g) const {
+    auto c00 = coefficients[0][0];
+    auto c01 = coefficients[0][1];
+    auto c02 = coefficients[0][2];
+    auto c03 = coefficients[0][3];
+    auto c10 = coefficients[1][0];
+    auto c11 = coefficients[1][1];
+    auto c12 = coefficients[1][2];
+    auto c20 = coefficients[2][0];
+    auto c21 = coefficients[2][1];
+    auto c30 = coefficients[3][0];
+
+    auto g0 = g.coefficients[0];
+    auto g1 = g.coefficients[1];
+
+    return Polynomial<3>({
+        c00 + c10*g0 + c20*g0*g0 + c30*g0*g0*g0,
+        c01 + c11*g0 + c21*g0*g0 + c10*g1 + 2*c20*g0*g1 + 3*c30*g0*g0*g1,
+        c02 + c12*g0 + c11*g1 + 2*c21*g0*g1 + c20*g1*g1 + 3*c30*g0*g1*g1,
+        c03 + c12*g1 + c21*g1*g1 + c30*g1*g1*g1,
+    });
 }
 
 template<size_t D>
