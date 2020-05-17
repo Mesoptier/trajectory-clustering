71123dcc122a5505058dac4671b4310ee5b0c027
Koen Klaren <koen.klaren@gmail.com>
2020-05-17 16:50:26 +0200

    Compute `l`, the ellipse shape parameter

---
+++ Google_tests/CellTest.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include "../src/IntegralFrechet/Cell.h"
+
+TEST(CellTest, EllipseShape) {
+    {
+        // Parallel, same direction
+        const Cell cell(
+            {0, 0}, {0, 0},
+            {1, 0}, {1, 0}
+        );
+        ASSERT_DOUBLE_EQ(cell.l, -1);
+    }
+    {
+        // Parallel, opposite direction
+        const Cell cell(
+            {0, 0}, {0, 0},
+            {1, 0}, {-1, 0}
+        );
+        ASSERT_DOUBLE_EQ(cell.l, 1);
+    }
+    {
+        // Orthogonal
+        const Cell cell(
+            {0, 0}, {0, 0},
+            {1, 0}, {0, 1}
+        );
+        ASSERT_DOUBLE_EQ(cell.l, 0);
+    }
+    {
+        // Somewhere in between
+        const Cell cell(
+            {0, 0}, {0, 0},
+            {1, 0}, {1, 1}
+        );
+        ASSERT_DOUBLE_EQ(cell.l, -0.70710678118654746);
+    }
+}
