333dd6b4d7188b627989a424c8977c83e85ca7a0
milutinB <milutin.brankovic3@gmail.com>
2021-10-05 17:05:08 +1100

    parameter selection experiments

--- main.cpp
+++ main.cpp
@@ -12,6 +12,6 @@
 int main() {
 
     experiments::characters_param_selection();
-    experiments::aic();
-    experiments::elbow_graph();
+    // experiments::aic();
+    // experiments::elbow_graph();
 }
