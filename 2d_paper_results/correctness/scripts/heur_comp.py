004a1c7f3c588929c33682c33cf2f0196e63a363
Milutin Brankovic <mica@192-168-1-100.tpgi.com.au>
2022-12-16 10:42:53 +1100

    commit before big refactor

--- 2d_paper_results/correctness/scripts/heur_comp.py
+++ 2d_paper_results/correctness/scripts/heur_comp.py
@@ -4,14 +4,17 @@ import pandas as pd
 pigeons_frame = pd.read_csv("2d_paper_results/correctness/pigeon_results.csv")
 characters_frame = pd.read_csv("2d_paper_results/correctness/character_results.csv")
 
+
 ax = pigeons_frame.plot.bar(rot=0)
-ax.set_ylabel("cdtw cost")
-ax.set_xlabel("pairs of pigeon trajectories")
+ax.legend(prop={'size': 14})
+ax.set_ylabel("cdtw cost", fontdict={'fontsize':14})
+ax.set_xlabel("pairs of pigeon trajectories", fontdict={'fontsize':14})
 fig = ax.get_figure()
 fig.savefig('2d_paper_results/correctness/figures/pigeons_comparison.png')
 
 ax = characters_frame.plot.bar(rot=0)
-ax.set_ylabel("cdtw cost")
-ax.set_xlabel("pairs of character trajectories")
+ax.legend(prop={'size': 14})
+ax.set_ylabel("cdtw cost", fontdict={'fontsize':14})
+ax.set_xlabel("pairs of character trajectories", fontdict={'fontsize':14})
 fig = ax.get_figure()
 fig.savefig('2d_paper_results/correctness/figures/characters_comparison.png')
